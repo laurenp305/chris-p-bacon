97a73e9452213ec4ca9c1c6bb097c9a187f3ec1a
laurenp305 <lamor1800@gmail.com>
2022-07-24 12:55:44 -0400

    added function for uv index and weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,73 @@
 function initPage () {
-    let cityName = document.getElementById(".city-name");
-    let currentPic = document.getElementById(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    let currentTemp = document.getElementById(".temperature").innerText = temp + "°F";
-    let currentHum = document.getElementById(".humidity").innerText= "Humidity: " + humidity + "%";
-    let currentWind = document.getElementById(".wind-speed").innerText = "Wind speed: " + speed + " MPH";
-    let currentUv = document.getElementById(".uv").innerText = "UV Index: " + uv;
-    let searchEl = document.getElementById("search-button");
-    let clearEl = document.getElementById("clear-history");
-    let searchHis = JSON.parse(localStorage.getItem)("search") || [];
+    let cityName = document.getElementById("#city-name");
+    let currentPic = document.getElementById("#icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    let currentTemp = document.getElementById("#temperature").innerText = temp + "°F";
+    let currentHum = document.getElementById("#humidity").innerText= "Humidity: " + humidity + "%";
+    let currentWind = document.getElementById("#wind-speed").innerText = "Wind speed: " + speed + " MPH";
+    let currentUv = document.getElementById("#uv").innerText = "UV Index: " + uv;
+    let searchEl = document.getElementById("#search-button");
+    let clearEl = document.getElementById("#clear-history");
+    let todaysWeather = document.getElementById("#todays-weather")
+    let searchHis = JSON.parse(localStorage.getItem)("#search") || [];
+
+    //API Key//
+    const APIKey = "84b79da5e5d7c92085660485702f4ce8";
+
+    function weather(city) {
+        //Retrieves weather info from API
+        const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        fetch(queryUrl)
+        .then(function (response) {
+            todaysWeather.classList;
+
+            //Considers the date to produce weather information
+            let currentDate = new Date(response.data.dt * 1000);
+            let day = currentDate.getDay();
+            let month = currentDate.getMonth() +1;
+            let year = currentDate.getFullYear();
+            cityName.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ")";
+            let currentPic = response.data.weather[0].icon;
+            currentPic.setAttribute("alt", response.data.weaher[0].description);
+            currentTemp.innerHTML = "Temperature:" + k2f(response.data.main.temp) + " &#176F";
+            currentHum.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWind.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
+
+        //UV index function
+        let latitude = response.data.cord.lat;
+        let longitude = response.data.cord.lon;
+        let UvQueryUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&cnt=1";
+        fetch(UvQueryUrl)
+            .then(function (response) {
+                let UvIndex = document.createElement("span");
+
+            //Color coding for UV: green is good, yellow is okay, red is bad
+            if (response.data[0].value < 4) {
+                UvIndex.setAttribute("class", "badge badge-success");
+            } 
+                else if (response.data[0].value < 8) {
+                    UvIndex.setAttribute("class", "badge badge-warning");
+                }
+            else {
+                UvIndex.setAttribute("class", "badge badge-danger");
+            }
+            console.log(response.data[0].value)
+            UvIndex.innerHTML = response.data[0].value;
+            currentUv.innerHTML = "UV Index: ";
+            currentUv.append(UvIndex);
+            });
+            
+            
+            
+
+        }
+    }
+
+
+
+
 }
+
 
 
 
